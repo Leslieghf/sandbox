8ee7635022f40f89eff765830c1578b2d17a682b
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2024-01-30 01:11:45 +0100

    WIP ECS Wrapper

---
+++ src/ECS/entity/mod.rs
@@ -0,0 +1,30 @@
+use serde::{Serialize, Deserialize};
+use std::fmt::Debug;
+use std::any::*;
+use bevy::ecs::entity::Entity as BevyEntity;
+
+#[derive(Send + Sync + Serialize + Deserialize<'static>, Default + Debug + Clone)]
+pub struct Entity {
+    bevy_entity: BevyEntity,
+    registered_components: Vec<Box<dyn Component>>,
+}
+
+impl Entity {
+    pub fn new() -> Entity {
+        Self {
+            bevy_entity: BevyEntity::new(),
+        }
+    }
+    
+    pub fn get_entity_id(&self) -> BevyEntity {
+        self.entity_id
+    }
+
+    pub fn register_component(&mut self, component: Box<dyn Component>) {
+        self.registered_components.push(component);
+    }
+
+    pub fn unregister_component(&mut self, component: Box<dyn Component>) {
+        self.registered_components.remove(component);
+    }
+}
